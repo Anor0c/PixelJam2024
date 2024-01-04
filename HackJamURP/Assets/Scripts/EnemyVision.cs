773791d80d4b67446b269743d2e44a5071ba8130
Ano <locorona.41@gmail.com>
2024-01-04 19:00:17 +0100

    StartEnemyVision/AddConePackage

---
+++ HackJamURP/Assets/Scripts/EnemyVision.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
