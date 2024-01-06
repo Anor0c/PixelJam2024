045cf435630534eedd875f17f7f49212c5144744
Ano <locorona.41@gmail.com>
2024-01-06 04:44:45 +0100

    Enemy Voit Et Tire

--- HackJamURP/Assets/Scripts/EnemyVision.cs
+++ HackJamURP/Assets/Scripts/EnemyVision.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EnemyVision : MonoBehaviour
 {
-
-    void Start()
+    [SerializeField] Transform canon;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float timeBetweenShots = 0.5f;
+    [SerializeField] float currentTimer = 0f;
+    [SerializeField] float visionDistance = 10f;
+    [SerializeField] float visionAngle = 45f;
+    [SerializeField] PlayerMove target; 
+    private void Start()
     {
-        
+        currentTimer = timeBetweenShots;
+        target = FindObjectOfType<PlayerMove>(); 
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (target == null)
+            return;
+        if (other.gameObject.tag == "Wall")
+            return;
+
+        var targetDir = target.transform.position - canon.position;
+        canon.forward = Vector3.MoveTowards(canon.forward, targetDir, 100);
+        /*if (canon.localEulerAngles.y < visionAngle || canon.localEulerAngles.y > -visionAngle)*/
+        var _checkLOS = Physics.Raycast(canon.position, targetDir, visionDistance);
+        Debug.Log(_checkLOS);
+        Debug.DrawRay(canon.position, targetDir, Color.red);
+        if (currentTimer <= 0f && _checkLOS)
+        {
+            Fire();
+            currentTimer = timeBetweenShots;
+        }
+        else if (_checkLOS)
+            currentTimer -= Time.deltaTime;
+        else
+            currentTimer = timeBetweenShots;
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        currentTimer = timeBetweenShots; 
+    }
+    public void Fire()
+    {
+        Instantiate(bullet, canon.position, canon.rotation); 
     }
 }
