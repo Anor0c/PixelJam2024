a3112ff7aa050e0d4747ed16dca6ded1884ccc6d
Ano <locorona.41@gmail.com>
2024-01-07 15:18:51 +0100

    Make Colors Button et Lazer Editable dans l'inspecteur

--- HackJamURP/Assets/Scripts/ButtonScript.cs
+++ HackJamURP/Assets/Scripts/ButtonScript.cs
@@ -1,23 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events; 
+using UnityEngine.Events;
+using UnityEditor; 
 
 public class ButtonScript : MonoBehaviour
 {
-    [SerializeField] LazerEnum color = LazerEnum.Red;
-    [SerializeField] Color meshColor = Color.red; 
     public UnityEvent<LazerEnum> onButtonPressed;
+    [SerializeField] LazerEnum lazerAffected = LazerEnum.Red;
+    [SerializeField] Color meshColor = Color.red; 
+ 
     private void Awake()
     {
-        GetComponent<MeshRenderer>().material.color = meshColor;   
+        onButtonPressed.AddListener(FindObjectOfType<LazerManager>().OnAnyButtonPressed); 
+    }
+    public void ColorMesh(Color _color)
+    {
+        GetComponent<MeshRenderer>().sharedMaterial.color = _color;
     }
     private void OnTriggerEnter(Collider _collision)
     {
         Debug.Log(_collision);
         if (_collision.gameObject.tag != "Bullet")
             return;
-        onButtonPressed.Invoke(color);
+        onButtonPressed.Invoke(lazerAffected);
     }
 
 }
+#if UNITY_EDITOR
+[CustomEditor(typeof(ButtonScript))]
+public class ButtonScriptEditor : Editor
+{
+    SerializedProperty editorMeshColor;
+    private void OnEnable()
+    {
+        editorMeshColor = serializedObject.FindProperty("meshColor"); 
+    }
+    public override void OnInspectorGUI()
+    {
+        ButtonScript button = (ButtonScript)target;
+        Color _newMeshColor = editorMeshColor.colorValue; 
+
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Apply color to button Mesh"))
+        {
+            button.ColorMesh(_newMeshColor); 
+        }
+    }
+}
+
+#endif 
--- HackJamURP/Assets/Scripts/LazerBehaviour.cs
+++ HackJamURP/Assets/Scripts/LazerBehaviour.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor; 
 [RequireComponent(typeof(SpriteRenderer))]
 [ExecuteInEditMode]
 public class LazerBehaviour : MonoBehaviour
 {
-    [SerializeField] public LazerEnum lazerColor = LazerEnum.Red;
     [SerializeField] Sprite[] lazerSprites;
+    [SerializeField] public LazerEnum lazerColor = LazerEnum.Red;
+
     private void Start()
     {
-        GetComponent<SpriteRenderer>().sprite = lazerSprites[(int)lazerColor]; 
+
+    }
+    public void SwitchSprite(int _lazerColorIndex)
+    {
+        GetComponent<SpriteRenderer>().sprite = lazerSprites[_lazerColorIndex]; 
+    }
+}
+[CustomEditor(typeof(LazerBehaviour))]
+public class LazerBehaviourEditor : Editor
+{
+    SerializedProperty editorColorEnum;
+
+    private void OnEnable()
+    {
+        editorColorEnum = serializedObject.FindProperty("lazerColor");
+    }
+    public override void OnInspectorGUI()
+    {
+        LazerBehaviour lazer = (LazerBehaviour)target; 
+        base.OnInspectorGUI();
+        int _newColorEnum = editorColorEnum.enumValueIndex; 
+        if(GUILayout.Button("Apply color to Lazer"))
+        {
+            lazer.SwitchSprite(_newColorEnum); 
+        }
     }
 }
